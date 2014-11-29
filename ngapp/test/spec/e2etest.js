7f0b1b98095526075771e2423cdfe8deffd0a7ed
tknemuru <kojikoji.max.5252@gmail.com>
2014-11-30 00:50:58 +0900

    e2etestをTypeScriptで書き直し

--- ngapp/test/spec/e2etest.js
+++ ngapp/test/spec/e2etest.js
@@ -1,10 +1,12 @@
-'use strict'
-describe('e2etest', function() {
-  beforeEach(function() {
-    browser.get('/#/');
-  });
-
-  it('this is e2e test', function() {
-      expect(true).toBe(true);
-  });
+/// <reference path="../../bower_components/DefinitelyTyped/jasmine/jasmine.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/angular-protractor/angular-protractor.d.ts" />
+'use strict';
+describe('e2eの動作テスト', function () {
+    beforeEach(function () {
+        browser.get('/#/');
+    });
+    it('e2eの基本的な動作確認', function () {
+        expect(true).toBe(true);
+    });
 });
+//# sourceMappingURL=e2etest.js.map
---
+++ ngapp/test/spec/e2etest.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../bower_components/DefinitelyTyped/jasmine/jasmine.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/angular-protractor/angular-protractor.d.ts" />
+
+'use strict';
+
+describe('e2eの動作テスト', () => {
+    beforeEach(() => {
+        browser.get('/#/');
+    });
+
+    it('e2eの基本的な動作確認', () => {
+        expect(true).toBe(true);
+    });
+});
